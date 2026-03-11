3fa9ef925a4ad50e90ef2e86fd4df492ba77971e
Ragu <111031929+Raghulands@users.noreply.github.com>
2026-03-11 16:35:47 +0530

    Add files via upload

---
+++ Python/pattern3.py
@@ -0,0 +1,9 @@
+for i in range(0,10):
+    for j in range(i,10):
+        print("*", end=" ")
+    print()
+for i in range(2,10):
+    for j in range(0,i):
+        print("*", end=" ")
+    print()
+
